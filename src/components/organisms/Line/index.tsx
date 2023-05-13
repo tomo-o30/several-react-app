ea4fa8bcf41ff2cd4a785bf1e9583e6f7e41ce3d
tomo-o30 <tomo.ogawa18@gmail.com>
2023-05-13 20:35:16 +0900

    wip: 質問用

--- src/components/organisms/Line/index.tsx
+++ src/components/organisms/Line/index.tsx
@@ -1,27 +1,38 @@
 import Signout from "../Signout";
 import { db } from "../../../firebase";
 import { useEffect, useState } from "react";
-import { collection, query, getDocs } from "firebase/firestore";
+import {
+  collection,
+  query,
+  getDocs,
+  CollectionReference,
+} from "firebase/firestore";
+
+type Message = {
+  createdAt: { seconds: number; nanoseconds: number };
+  test: string;
+};
 
 const Line = () => {
-  const [messages, setMessages] = useState();
+  const [messages, setMessages] = useState<Message[] | undefined>(undefined);
   useEffect(() => {
     const getMessages = async () => {
-      const q = query(collection(db, "messages"));
+      const q = query(
+        collection(db, "messages") as CollectionReference<Message>
+      );
       const querySnapshot = await getDocs(q);
 
       querySnapshot.forEach((doc) => {
-        setMessages(doc.data());
+        setMessages([messages, doc.data()]);
       });
     };
     getMessages();
   }, []);
   return (
     <>
-      {console.log(messages)}
       <Signout />
       <div>
-        {/* {messages.map((message) => {
+        {/* {messages ? messages.map((message: ) => {
           <div>
             <div key={message.id}>{message}</div>
           </div>;
