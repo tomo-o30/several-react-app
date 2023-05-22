c9b9356969f5eb818a7598042cc9793ae38f0d7c
tomo-o30 <tomo.ogawa18@gmail.com>
2023-05-23 01:33:00 +0900

    add: scssの導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,22 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
-// 全体にかけるCSS
-import "./app.css";
+// 全体にかけるSCSS
+import "./app.scss";
 
 const App = ({ Component, pageProps }: AppProps) => (
   <>
-    <Head>
-      <title>to-do-app</title>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, shrink-to-fit=no"
-      />
-      {/* <link rel="shortcut icon" href="/favicon.png" key="shortcutIcon" /> */}
-      {/* <link rel="manifest" href="/manifest.json" /> */}
-    </Head>
-    <Component {...pageProps} />
+    <div className="App">
+      <Head>
+        <title>to-do-app</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {/* <link rel="shortcut icon" href="/favicon.png" key="shortcutIcon" /> */}
+        {/* <link rel="manifest" href="/manifest.json" /> */}
+      </Head>
+      <Component {...pageProps} />
+    </div>
   </>
 );
 
