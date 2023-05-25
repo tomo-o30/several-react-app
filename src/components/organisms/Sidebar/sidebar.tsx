78b0a4d2df6c96920df6a060aff49c983edcdc22
tomo-o30 <tomo.ogawa18@gmail.com>
2023-05-26 00:47:45 +0900

    fix: footer作成

--- src/components/organisms/Sidebar/sidebar.tsx
+++ src/components/organisms/Sidebar/sidebar.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import styles from "./sidebar.module.css";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from "@mui/icons-material/Add";
+import MicIcon from "@mui/icons-material/Mic";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
+import SettingsIcon from "@mui/icons-material/Settings";
 import SidebarChannel from "../SidebarChannel/sidebarChannel";
 
 const Sidebar = () => {
@@ -33,6 +36,21 @@ const Sidebar = () => {
           </div>
           <div className={styles.idebarChannelList}>
             <SidebarChannel />
+          </div>
+
+          <div className={styles.sidebarFooter}>
+            <div className={styles.sidebarAccount}>
+              <img src="./icon.png" alt="" />
+              <div className={styles.accountName}>
+                <h4 className={styles.h4}>ああああ</h4>
+                <span className={styles.span}>いいいい</span>
+              </div>
+            </div>
+            <div className={styles.sidebarVoice}>
+              <MicIcon />
+              <HeadphonesIcon />
+              <SettingsIcon />
+            </div>
           </div>
         </div>
       </div>
