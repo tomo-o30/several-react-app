9988d5aa662577ca30166479e4be60df36703463
tomo-o30 <tomo.ogawa18@gmail.com>
2023-06-01 00:25:17 +0900

    fix: ErrorBoundary修正

---
+++ src/utils/ErrorFallBack.tsx
@@ -0,0 +1,13 @@
+import { ErrorBoundary, FallbackProps } from "react-error-boundary";
+
+export function ErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
+  // Call resetErrorBoundary() to reset the error boundary and retry the render.
+
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: "red" }}>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+}
