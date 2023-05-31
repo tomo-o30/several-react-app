e6912fc3183e6a5dcd39158b89322513620ddff4
tomo-o30 <tomo.ogawa18@gmail.com>
2023-06-01 00:24:42 +0900

    fix: firebase 機能追加

--- src/components/organisms/Sidebar/sidebar.tsx
+++ src/components/organisms/Sidebar/sidebar.tsx
@@ -1,13 +1,28 @@
-import React from "react";
-import styles from "./sidebar.module.css";
+import React, { useEffect } from "react";
+import styles from "./sidebar.module.scss";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from "@mui/icons-material/Add";
 import MicIcon from "@mui/icons-material/Mic";
 import HeadphonesIcon from "@mui/icons-material/Headphones";
 import SettingsIcon from "@mui/icons-material/Settings";
 import SidebarChannel from "../SidebarChannel/sidebarChannel";
+import { auth, db } from "../../../firebase";
+import { useAppSelector } from "../../../app/hooks";
+import { collection, onSnapshot, query } from "firebase/firestore";
+import {} from "firebase/firestore";
 
 const Sidebar = () => {
+  const user = useAppSelector((state) => state.user);
+
+  const q = query(collection(db, "channels"));
+
+  useEffect(() => {
+    onSnapshot(q, (querySnapshot) => {
+      const channelsResults = [];
+      querySnapshot.docs.forEach((doc) => console.log(doc));
+    });
+  }, []);
+
   return (
     <div className={styles.sidebar}>
       {/* {sidebarLeft} */}
@@ -39,10 +54,12 @@ const Sidebar = () => {
           </div>
           <div className={styles.sidebarFooter}>
             <div className={styles.sidebarAccount}>
-              <img src="./icon.png" alt="" />
+              <img src={user?.photo} onClick={() => auth.signOut()} alt="" />
               <div className={styles.accountName}>
-                <h4 className={styles.h4}>ああああ</h4>
-                <span className={styles.span}>いいいい</span>
+                <h4 className={styles.h4}>{user?.displayName}</h4>
+                <span className={styles.span}>
+                  #{user?.uid.substring(0, 4)}
+                </span>
               </div>
             </div>
             <div className={styles.sidebarVoice}>
