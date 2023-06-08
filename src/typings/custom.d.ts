797e07978e3456d58f88eebf0bce58e4fa2e452f
小川智裕 <tomo.create30@gmail.com>
2023-06-08 22:29:21 +0900

    fix: 型

--- src/typings/custom.d.ts
+++ src/typings/custom.d.ts
@@ -1,7 +1,6 @@
-import React from 'react';
 
 
-declare module 'React' {
+declare module 'react' {
   interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
     jsx?: boolean;
     global?: boolean;
